e6b75f6f62209a857f59015655868d250f73a282
SiHaythem420 <SiHaythem420>
2026-03-14 19:47:55 +0100

    API_Gateway

---
+++ services/API_Gatewya/src/main/java/serenity/api_gatewya/ApiGatewyaApplication.java
@@ -0,0 +1,13 @@
+package serenity.api_gatewya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewyaApplication.class, args);
+    }
+
+}
